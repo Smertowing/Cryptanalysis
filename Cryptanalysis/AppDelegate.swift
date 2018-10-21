ff4321b7faf74c8111fd15d87a07baee20218d8e
Kiryl <Kiryl.co@gmail.com>
2018-10-21 18:22:48 +0300

    Initial Commit

---
+++ Cryptanalysis/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Cryptanalysis
+//
+//  Created by Kiryl Holubeu on 10/21/18.
+//  Copyright © 2018 Kiryl Holubeu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Cryptanalysis/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Cryptanalysis
+//
+//  Created by Kiryl Holubeu on 10/21/18.
+//  Copyright © 2018 Kiryl Holubeu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
